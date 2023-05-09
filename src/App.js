0202e01de62f8e4fc74fcba9d49d16a3a233cfc1
FinalProject <130982229+FinalProjectReact@users.noreply.github.com>
2023-05-09 21:31:59 +0300

    start to style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import React from 'react';
 import "./App.css";
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
 import Body from "./components/body/body";
+import { Route, Router, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+    {/* <Router> */}
       <Header />
+      {/* <Routes>
+        <Route path='/' exact component={Header} />
+        <Route path='/about' component={Header} />
+        <Route path='/events' component={Header} />
+        <Route path='/annual' component={Header} />
+        <Route path='/team' component={Header} />
+        <Route path='/blogs' component={Header} />
+        <Route path='/sign-up' component={Header} />
+      </Routes> */}
       <Body />
       <Footer />
+    {/* </Router> */}
+
     </div>
   );
 }
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,36 +1,38 @@
-import React from "react";
-import { Link } from "react-router-dom";
 
-export default function Header() {
-  return (
-    <div>
-      <h1>ברוכים הבאים למאגר הלומדים</h1>
-
-      <>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/recomment">חוות דעת</Link>
-            </li>
-            {/* <li>
-            <Link to="/login">Login</Link>
-          </li>
-
-          <li>
-            <Link to="/loginOut">LoginOut</Link>
-          </li> */}
-          </ul>
-        </nav>
-        {/* <button onClick={loginOut}>הרשמה למורה</button> */}
-      </>
-
-      {/* <Login />
-
+import React from 'react';
+import {Nav, NavLink, Bars, NavMenu, NavBtn, NavBtnLink,} from '../NavbarElements';
   
-    <Outlet /> */}
-    </div>
+const Header = () => {
+  return (
+    <>
+      <Nav>
+        <Bars />
+        <img id='logo' alt='logo' src='.../picthers/logo.png'/>
+        <NavMenu>
+          <NavLink to='/בית' activeStyle>
+            בית
+          </NavLink>
+          <NavLink to='/חומרי_לימוד' activeStyle>
+            חומרי לימוד
+          </NavLink>
+          <NavLink to='/אודות' activeStyle>
+            אודות
+          </NavLink>
+          <NavLink to='/צור_קשר' activeStyle>
+            צור קשר
+          </NavLink>
+          <NavLink to='/בלוג' activeStyle>
+            בלוג
+          </NavLink>
+
+        </NavMenu>
+        <NavBtn>
+          <NavBtnLink to='/sign-in'>התחברות</NavBtnLink>
+          <NavBtnLink to='/sign-out'>יצירת פרופיל מורה חדש</NavBtnLink>
+        </NavBtn>
+      </Nav>
+    </>
   );
-}
+};
+  
+export default Header;
