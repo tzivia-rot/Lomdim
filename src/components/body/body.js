a5745535e8274c2a8e4d8afcf922ac8a8771560d
FinalProject <130982229+FinalProjectReact@users.noreply.github.com>
2023-07-25 10:09:17 +0300

    //

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -4,13 +4,13 @@ import Home from "../home/home";
 import Login from "../Login/login";
 import Register from "../Register/register";
 import Blog from "../blog/blog";
-import ContactUs from "../contactUs/contactUs";
 import About from "../about/about";
 import PersonalDetails from "../Register/professionalDetails";
 import HowToLearnEnglish from "../blog/items-blogs/howToLearnEnglish";
 import AccountPupil from "../accountPupil/accountPupil";
 import AccountTeacher from "../accountTeacher/accountTeacher";
 import PageTeacher from "../pageTeacher/pageTeacher";
+//import ContactUs from "../contactUs/contactUs";
 
 export default function Body() {
   return (
@@ -19,7 +19,7 @@ export default function Body() {
         <Route path="/" element={<Home />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/howToLearnEnglish" element={<HowToLearnEnglish />} />
-        <Route path="/contact_us" element={<ContactUs />} />
+        {/* <Route path="/contact_us" element={<ContactUs />} /> */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/personal_details" element={<PersonalDetails />} />
--- src/components/contactUs/contactUs.js
+++ src/components/contactUs/contactUs.js
@@ -1,34 +1,7 @@
-import React, { useState } from "react";
-import './contactUs.css'
+import React from 'react'
 
 export default function ContactUs() {
-const [email, setEmail] = useState();
-const [detail, setDetail] = useState();
-
   return (
-    <div className="contact-wrapper">
-      <h1>פנייה לצוות האתר</h1>
-      <p>אם מצאת טעות או שאתה רוצה שנוסיף משהו, שלח לנו הודעה!</p>
-      <div>
-        <p>מייל לחזרה*</p>
-        <input
-          type="text"
-          autoComplete="on"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-          required
-        >
-        </input>
-        <p>הודעה:*</p>
-        <textarea
-        rows="5"
-        onChange={(e) => setDetail(e.target.value)}
-        value={detail}
-        required
-        >
-        </textarea>
-        <button className="btn-send">שליחה</button>
-      </div>
-    </div>
-  );
+    <div>צור קשר</div>
+  )
 }
